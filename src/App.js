4c8d6d2502f48a6a9ed514f64ca4a35732423d12
frontcatbackend <78782041+frontcatbackend@users.noreply.github.com>
2022-03-15 13:07:51 +1200

    server + client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [listOfPost, setListOfPost] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/posts").then((response) => {
+      console.log(response.data);
+      setListOfPost(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {listOfPost.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postText}</div>
+            <div className="body">{value.username}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
