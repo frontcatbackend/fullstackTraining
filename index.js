8f240ae64cc3b3fbf6c11386d37f177ae6fa7a99
frontcatbackend <78782041+frontcatbackend@users.noreply.github.com>
2022-03-15 12:19:02 +1200

    create + find all pasts

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const db = require('./models/index')
 
+app.use(express.json())
+
+//Routers
+const postRouter = require('./routes/Posts')
+app.use("/posts", postRouter)
+
 db.sequelize.sync().then(()=>{
     app.listen(5000, ()=>{
         console.log(`Server has been started `)
